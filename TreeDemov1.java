31332a938ecba31348ade30b94c39018648811b7
Ameer Aldarraji <ameeraldarraji@Ameers-MacBook-Pro.local>
2026-04-15 20:52:10 -0700

    Implemented preOrderTraversal

--- TreeDemov1.java
+++ TreeDemov1.java
@@ -57,10 +57,14 @@ class Node{
 	   pre-order traversal
 	   Prints the value of every node preorder
 	   */
-	   public void preOrderTraversal(Node root){
-		//implement in here
-		   
-	   }
+	public void preOrderTraversal(Node root){
+    if(root == null){
+        return;
+    }
+    System.out.print(root.value + " ");
+    preOrderTraversal(root.left);
+    preOrderTraversal(root.right);
+}
 
 	   
 	   
